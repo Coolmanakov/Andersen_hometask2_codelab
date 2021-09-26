1ecf916a68738a6a1a47848d01003c41c4387611
Coolmanakov <kulmanakovdenis@yandex.ru>
2021-09-26 19:02:00 +0300

    HomeTask activity_intent, used ActivityResultContract for transactions

--- app/src/main/java/com/example/helloworld/SecondActivity.kt
+++ app/src/main/java/com/example/helloworld/SecondActivity.kt
@@ -2,16 +2,25 @@ package com.example.helloworld
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.helloworld.hello_toast.HelloToast
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
+    var mCount : TextView? = null
     companion object{
         private  val LOG_TAG = "Main Activity"
+        val REPLY = "com.example.helloworld.SecondActivity"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mCount = findViewById(R.id.count_hello)
+
+        val count = intent.getCharSequenceExtra(HelloToast.SEND_MESSAGE)
+        mCount?.text = count
 
         Log.d(LOG_TAG, "Main Activity calling onCreate() ")
     }
--- app/src/main/java/com/example/helloworld/hello_toast/HelloToast.kt
+++ app/src/main/java/com/example/helloworld/hello_toast/HelloToast.kt
@@ -1,15 +1,27 @@
 package com.example.helloworld.hello_toast
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 import com.example.helloworld.R
+import com.example.helloworld.SecondActivity
 
 class HelloToast : AppCompatActivity() {
     private var mCount = 0
     private var mShowCount : TextView? = null
+
+    var activityLauncher = registerForActivityResult(SecondActivityContract()){result ->
+        mShowCount?.text = result.toString()
+    }
+
+    companion object{
+        val SEND_MESSAGE = "com.example.helloworld.HelloToast"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello_toast)
@@ -17,14 +29,34 @@ class HelloToast : AppCompatActivity() {
         mShowCount = findViewById(R.id.show_count)
     }
 
-    fun showToast(view: View) {
-        val toast : Toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT)
-        toast.show()
-    }
+//    fun showToast(view: View) {
+//        val toast : Toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT)
+//        toast.show()
+//    }
+
     fun countUp(view: View) {
         mCount++
         mShowCount?.apply {
             text = "$mCount"
         }
+    }
+
+    fun sayHello(view: View) {
+        val number = mShowCount?.text
+        activityLauncher.launch(number)
+    }
+
+    class SecondActivityContract : ActivityResultContract<CharSequence?, Int>(){
+        override fun createIntent(context: Context, input: CharSequence?): Intent {
+            return Intent(context, SecondActivity::class.java)
+                .putExtra(SEND_MESSAGE, input)
+        }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): Int? = when{
+            resultCode != Activity.RESULT_OK -> 0
+            else -> intent?.getIntExtra(SecondActivity.REPLY, 0)
+
+        }
+
     }
 }
